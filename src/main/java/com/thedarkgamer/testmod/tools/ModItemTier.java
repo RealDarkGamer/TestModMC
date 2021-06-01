62121ef8029d5b8c2b20a3520cf286cdfa55ff59
Jonathan Morton <jonathan.x.morton@outlook.com>
2021-06-01 14:45:40 -0500

    Add Copper Sword and Redid Copper Ingot

---
+++ src/main/java/com/thedarkgamer/testmod/tools/ModItemTier.java
@@ -0,0 +1,57 @@
+package com.thedarkgamer.testmod.tools;
+
+import com.thedarkgamer.testmod.util.RegistryHandler;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+
+    COPPER(2, 650, 6.5f, 3.0f, 11, ()-> {
+        return Ingredient.fromItems(RegistryHandler.COPPERSWORD.get());
+    });
+
+    private final int harvestLevel, maxUses, enchantability;
+    private final float efficiency, attackDamage;
+    private final Supplier<Ingredient> repairMaterial;
+
+    ModItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.get();
+    }
+}
--- src/main/java/com/thedarkgamer/testmod/util/RegistryHandler.java
+++ src/main/java/com/thedarkgamer/testmod/util/RegistryHandler.java
@@ -4,9 +4,12 @@ import com.thedarkgamer.testmod.TestMod;
 import com.thedarkgamer.testmod.blocks.BlockItemBase;
 import com.thedarkgamer.testmod.blocks.CopperBlock;
 import com.thedarkgamer.testmod.items.ItemBase;
+import com.thedarkgamer.testmod.tools.ModItemTier;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -24,6 +27,11 @@ public class RegistryHandler {
 
     //Items
     public static final RegistryObject<Item> COPPERINGOT = ITEMS.register("copper_ingot", ItemBase::new);
+
+    //Tools
+    public static final RegistryObject<SwordItem> COPPERSWORD = ITEMS.register("copper_sword", () ->
+      new SwordItem(ModItemTier.COPPER, 3, -2.4f, new Item.Properties().group(TestMod.TAB).rarity(Rarity.UNCOMMON))
+    );
 
     //Blocks
     public static final RegistryObject<Block> COPPERBLOCK = BLOCKS.register("copper_block", CopperBlock::new);
